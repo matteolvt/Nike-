dde93c0ede5655fe9d3de02b6dd878b0d6a343d0
Tom Julliat <146728179+ShayyNwE@users.noreply.github.com>
2024-10-13 21:35:13 +0200

    :sparkles: Ajout panier

--- script.js
+++ script.js
@@ -12,6 +12,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
+const cartButton = document.getElementById('cart-button');
+const cartPanel = document.getElementById('cart-panel');
+const closeCartButton = document.getElementById('close-cart');
+
+// Quand on clique sur l'icône du panier, on l'ouvre
+cartButton.addEventListener('click', function() {
+    cartPanel.classList.add('active'); // Ajouter la classe active pour déclencher l'animation d'ouverture
+});
+
+// Quand on clique sur le bouton "Fermer"
+closeCartButton.addEventListener('click', function() {
+    cartPanel.classList.add('closing'); // Ajoute la classe pour commencer à glisser le panier vers la droite
+    cartPanel.classList.remove('active'); // Retire la classe active (pour retirer le slide-in)
+    
+    setTimeout(() => {
+        cartPanel.classList.remove('closing'); // Une fois l'animation terminée, retirer la classe 'closing'
+    }, 500); // Correspond à la durée de la transition CSS (500ms)
+});
+
 function toggleFilter(id) {
     const content = document.getElementById(id);
     const button = content.previousElementSibling; // Récupère le bouton du filtre
