52ea66250dfc2bbbe804efb3e95c07f8569bf729
matteolvt <146728526+matteolvt@users.noreply.github.com>
2024-10-13 18:52:22 +0200

    :sparkles:ajout du menu hamberger

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var swiper = new Swiper('.swiper-container', {
-        loop: true, // Permet de faire boucler les slides
-        autoplay: {
-            delay: 3000, // 3 secondes entre les slides
-        },
-        pagination: {
-            el: '.swiper-pagination', // Pagination (petits points en bas)
-            clickable: true, // Rendre les points cliquables
-        },
-    });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     var swiper = new Swiper('.swiper-container', {
+//         loop: true, // Permet de faire boucler les slides
+//         autoplay: {
+//             delay: 3000, // 3 secondes entre les slides
+//         },
+//         pagination: {
+//             el: '.swiper-pagination', // Pagination (petits points en bas)
+//             clickable: true, // Rendre les points cliquables
+//         },
+//     });
+// });
+
+
+
