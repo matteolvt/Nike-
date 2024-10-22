7983166ee5cc84ad49dd252c80b79c490bdf0adf
matteolvt <146728526+matteolvt@users.noreply.github.com>
2024-10-22 11:45:40 +0200

    modification homepage

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -9,9 +9,15 @@ const ItemsList = () => {
           <a href="#">Shoes -&gt;</a>
         </div>
         <div className="shoes-items">
-          <div className="shoes-item1"></div>
-          <div className="shoes-item2"></div>
-          <div className="shoes-item3"></div>
+          <div className="shoes-item1">
+            <img src="./images/W+AIR+FORCE+1+'07+FLYEASE.png" alt="" />
+          </div>
+          <div className="shoes-item2">
+            <img src="./images/W+NIKE+DUNK+LOW+NEXT+NATURE.png" alt="" />
+          </div>
+          <div className="shoes-item3">
+            <img src="./images/JR+ZM+VAPOR+16+PRO+FG.png" alt="" />
+          </div>
         </div>
       </div>
 
@@ -20,15 +26,21 @@ const ItemsList = () => {
           <a href="#">Clothes -&gt;</a>
         </div>
         <div className="clothes-items">
-          <div className="clothes-item1"></div>
-          <div className="clothes-item2"></div>
-          <div className="clothes-item3"></div>
+          <div className="clothes-item1">
+            <img src="./images/FCB+M+NK+DF+JSY+SS+STAD+HM.png" alt="" />
+          </div>
+          <div className="clothes-item2">
+            <img src="./images/FCB+M+NK+DF+JSY+SS+STAD+3R.png" alt="" />
+          </div>
+          <div className="clothes-item3">
+            <img src="./images/FCB+W+NK+DFADV+STRK+EL+DRILT+K.png" alt="" />
+          </div>
         </div>
       </div>
 
       <div className="news">
         <div className="news-title">
-          <a href="#">News</a>
+          {/* <a href="#">News</a> */}
         </div>
         <div className="news-items">
           <div className="news-item1"></div>
